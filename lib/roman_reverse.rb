4018009c981d032230cc8ba3d19ce2e7598c4d59
Kyle Boe & Michael Derfler <student@epicodus.com>
2014-07-22 15:32:31 -0700

    initial

---
+++ lib/roman_reverse.rb
@@ -0,0 +1,15 @@
+def roman_reverse(number)
+  numeral_hash = {
+I  =>     1
+V  =>     5
+X  =>     10
+L  =>     50
+C  =>     100
+D  =>     500
+M  =>     1,000
+}
+  new_arr = []
+  numeral_arr = number.split("")
+
+
+end
